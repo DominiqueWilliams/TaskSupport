90ebb45f63568d279d8312ddbaf07bef71bfe1ba
Diandre <410421258@gms.ndhu.edu.tw>
2019-07-25 11:57:34 -0400

    Activated Client Button (Untested)

--- app/src/main/java/com/buildncode/tasksupport/Page0Fragment.java
+++ app/src/main/java/com/buildncode/tasksupport/Page0Fragment.java
@@ -1,15 +1,18 @@
 package com.buildncode.tasksupport;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.buildncode.tasksupport.Client.Client;
 import com.buildncode.tasksupport.R;
 
 public class Page0Fragment extends Fragment {
-
+Button mClientButton;
 
     public Page0Fragment() {
         // Required empty public constructor
@@ -21,7 +24,13 @@ public class Page0Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_worksheet0, container, false);
-
+        mClientButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Client.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
