70cf23148a71165e3adb7c409a32d4e5079bdc9d
Diandre <410421258@gms.ndhu.edu.tw>
2019-07-26 10:00:10 -0400

    -Stuff

--- app/src/main/java/com/buildncode/tasksupport/Technician/Page2Fragment.java
+++ app/src/main/java/com/buildncode/tasksupport/Technician/Page2Fragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.buildncode.tasksupport.R;
 
@@ -13,6 +15,9 @@ import static com.buildncode.tasksupport.Technician.TechnicianGuideActivity.vwPa
 
 public class Page2Fragment extends Fragment {
     Button mBack, mForward;
+    RadioButton mStatusPendingRadio, mStatusAssignedRadio,mStatusPlannedRadio, mStatusSolvedRadio;
+    RadioButton mUrgencyVeryHigh, mUrgencyHigh,mUrgencyMedium,mUrgencyVeryLow,mUrgencyLow;
+    RadioGroup mStatusGroup, mUrgencyGroup;
 
     public Page2Fragment() {
         // Required empty public constructor
@@ -25,12 +30,32 @@ public class Page2Fragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_worksheet2, container, false);
         mBack = view.findViewById(R.id.back);
         mForward = view.findViewById(R.id.forward);
+        //Radio Buttons
+
+        mStatusPendingRadio = view.findViewById(R.id.pending);
+        mStatusAssignedRadio = view.findViewById(R.id.assigned);
+        mStatusPlannedRadio = view.findViewById(R.id.planned);
+        mStatusSolvedRadio = view.findViewById(R.id.solved);
+        mUrgencyVeryHigh = view.findViewById(R.id.very_high);
+        mUrgencyHigh = view.findViewById(R.id.high);
+        mUrgencyMedium = view.findViewById(R.id.medium);
+        mUrgencyLow = view.findViewById(R.id.low);
+        mUrgencyVeryLow = view.findViewById(R.id.very_low);
+        //Radio Group
+        mStatusGroup = view.findViewById(R.id.status_radio_group);
+        mUrgencyGroup = view.findViewById(R.id.urgency_radio_group);
+        mStatusGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                
+            }
+        });
         // For scrolling to next item
         mForward.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                vwPager.setCurrentItem(getNextPossibleItemIndex(1), true);
+        vwPager.setCurrentItem(getNextPossibleItemIndex(1), true);
             }
         });
 
--- app/src/main/java/com/buildncode/tasksupport/Technician/Page3Fragment.java
+++ app/src/main/java/com/buildncode/tasksupport/Technician/Page3Fragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.buildncode.tasksupport.R;
+import com.github.gcacace.signaturepad.views.SignaturePad;
 
 import static com.buildncode.tasksupport.Technician.TechnicianGuideActivity.vwPager;
 
@@ -17,7 +18,7 @@ import static com.buildncode.tasksupport.Technician.TechnicianGuideActivity.vwPa
  * A simple {@link Fragment} subclass.
  */
 public class Page3Fragment extends Fragment {
-    Button mBack, mForward;
+    Button mBack, mClear;
 
 
     public Page3Fragment() {
@@ -37,6 +38,31 @@ public class Page3Fragment extends Fragment {
             @Override
             public void onClick(View view) {
                 vwPager.setCurrentItem(getNextPossibleItemIndex(-1), true);
+            }
+        });
+        final SignaturePad tech_signaturePad =  view.findViewById(R.id.tech_signaturePad);
+        mClear = view.findViewById(R.id.clear);
+        tech_signaturePad.setOnSignedListener(new SignaturePad.OnSignedListener() {
+
+            @Override
+            public void onStartSigning() {
+                //Event triggered when the pad is touched
+            }
+
+            @Override
+            public void onSigned() {
+                //Event triggered when the pad is signed
+            }
+
+            @Override
+            public void onClear() {
+                //Event triggered when the pad is cleared
+            }
+        });
+        mClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tech_signaturePad.clear();
             }
         });
         return view;
